224b478d60807054ff464aa373783773ae9b5eae
Panlichen <plch368@qq.com>
2016-12-14 23:08:40 +0800

    FlowGraph built for sp2k

--- spiglet/spiglet2kanga/visitor/VisitorBuildFlowGraph.java
+++ spiglet/spiglet2kanga/visitor/VisitorBuildFlowGraph.java
@@ -228,10 +228,121 @@ public class VisitorBuildFlowGraph extends DepthFirstVisitor{
 	*/
 	public void visit(StmtExp n)
 	{
+		this.curTI = null;
+		this.endOfStmt = false;
+		n.f3.accept(this);
+		this.endOfStmt = true;
+		if(this.curTI != null)
+		{
+			this.curFG = new FlowGraph(this.curTI, this.curNumPara);
+			this.curTI = null;
+			this.curNumPara = 0;
+		}
+		else
+		{
+			this.curFG = new FlowGraph(null, this.curNumPara);
+			this.curNumPara = 0;
+		}
 		
+		this.needToBuildBB = true;
+		this.isLinkedToPre = false;
+		n.f1.accept(this);
 	}
 	
+	/**
+	* f0 -> "CALL"
+	* f1 -> SimpleExp()
+	* f2 -> "("
+	* f3 -> ( Temp() )*
+	* f4 -> ")"
+	*/
+	public void visit(Call n)
+	{
+		n.f1.accept(this);
+		int notPara = this.curStmt.size();
+		n.f3.accept(this);
+		this.curFG.update_maxCalledNumPara(this.curStmt.size() - notPara);
+	}
 	
+	/**
+	* f0 -> "HALLOCATE"
+	* f1 -> SimpleExp()
+	*/
+	public void visit(HAllocate n)
+	{
+		n.f0.accept(this);
+		n.f1.accept(this);
+	}
+	
+	/**
+	* f0 -> Operator()
+	* f1 -> Temp()
+	* f2 -> SimpleExp()
+	*/
+	public void visit(BinOp n)
+	{
+		n.f0.accept(this);
+		n.f1.accept(this);
+		n.f2.accept(this);
+	}
+	
+	/**
+	* f0 -> "LT"
+	*       | "PLUS"
+	*       | "MINUS"
+	*       | "TIMES"
+	*/
+	public void visit(Operator n)
+	{
+		n.f0.accept(this);
+	}
+	
+	/**
+	* f0 -> Temp()
+	*       | IntegerLiteral()
+	*       | Label()
+	*/
+	public void visit(SimpleExp n)
+	{
+		this.needToRecord++;
+		n.f0.accept(this);
+		this.needToRecord--;
+	}
+		
+	/**
+	* f0 -> "TEMP"
+	* f1 -> IntegerLiteral()
+	*/
+	public void visit(Temp n)
+	{
+		int num = Integer.parseInt(n.f1.f0.tokenImage);
+		this.curTI = new TempInfo(num);
+		if(this.needToRecord > 0)
+			this.curStmt.addElement(this.curTI);
+	}
+	
+	/**
+	* f0 -> <INTEGER_LITERAL>
+	*/
+	public void visit(IntegerLiteral n)
+	{
+		n.f0.accept(this);
+	}
+	
+	/**
+	* f0 -> <INTEGER_LITERAL>
+	*/
+	public void visit(Label n)
+	{
+		if(this.endOfStmt)
+		{
+			this.curBB = new BasicBlock();
+			this.curBB.set_block_label(n.f0.tokenImage);
+			this.curFG.add_BB(curBB, isLinkedToPre);
+			this.needToBuildBB =  false;
+			this.isLinkedToPre = true;
+		}
+	}
 	
 	
 	
